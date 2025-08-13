dcb55ae378fb3181307c4f3b1c2735c256145f94
saikiran121111 <saikiranvsk3@gmail.com>
2025-08-13 16:45:21 +0530

    Update by adding tailwind page homepage

--- src/components/portfolio/PortfolioName.tsx
+++ src/components/portfolio/PortfolioName.tsx
@@ -7,7 +7,10 @@ interface PortfolioNameProps {
 export async function PortfolioName({ className }: PortfolioNameProps) {
   const { name } = await fetchUserPortfolio();
   return (
-    <h1 className={className ? className : "text-3xl font-bold"}>
+    <h1
+      className=
+        "text-2xl font-bold tracking-tight md:text-4xl desktop:text-5xl md:leading-tight mt-[402px] sm:mt-0"
+    >
       {name}
     </h1>
   );
